15db906dc67f46b5a12354652438dadca234a041
ladyada <limor@ladyada.net>
2018-12-08 16:41:34 -0500

    working code - still needs some error checking/retry logic im sure

--- adafruit_espatcommands.py
+++ adafruit_espatcommands.py
@@ -19,7 +19,7 @@ class espatcommands:
 
     @property
     def mode(self):
-        reply = self.at_response("AT+CWMODE_CUR?", timeout=0.1).strip(b'\r\n')
+        reply = self.at_response("AT+CWMODE_CUR?", timeout=0.5).strip(b'\r\n')
         if not reply.startswith(b'+CWMODE_CUR:'):
             raise RuntimeError("Bad response")
         return int(reply[12:])
@@ -42,15 +42,16 @@ class espatcommands:
         self._versionstring = vers[11:]
         return self._versionstring
 
-    def get_my_ip(self):
-        reply = at_response("AT+CIFSR")
-        m = ure.match("\+CIFSR:STAIP,\"([0-9\.]+)", reply)
-        if m:
-            return m.group(1).decode('utf-8')
+    @property
+    def local_ip(self):
+        reply = self.at_response("AT+CIFSR").strip(b'\r\n')
+        for s in reply.split(b'\r\n'):
+            if s and s.startswith(b'+CIFSR:STAIP,"'):
+                return s[14:-1].decode('utf-8')
         raise RuntimeError("Couldn't find IP address")
 
     def join_AP(self, ssid, password):
-        reply = at_response('AT+CWJAP="'+ssid+'","'+password+'"')
+        reply = self.at_response('AT+CWJAP="'+ssid+'","'+password+'"', timeout=10)
         if not "WIFI CONNECTED" in reply:
             raise RuntimeError("Couldn't connect to WiFi")
         if not "WIFI GOT IP" in reply:
@@ -75,41 +76,44 @@ class espatcommands:
                     APs.append(AP)
             return APs
 
-    def at_response(self, at_cmd, timeout=10):
-        time.sleep(1)
-        if self._debug:
-            print("--->", at_cmd)
-        #self._uart.reset_input_buffer()
-        self._uart.write(at_cmd.encode('utf-8'))
-        self._uart.write(b'\x0d\x0a')
-        time.sleep(0.1)
-        #uart.timeout = timeout
-        #print(uart.readline())  # read echo and toss
-        t = time.monotonic()
-        response = b''
-        while (time.monotonic() - t) < timeout:
-            response += self._uart.read(self._uart.in_waiting)
-            if response[-4:] == b'OK\r\n':
-                break
-        # eat beginning \n and \r
-        if self._debug:
-            print("<---", response)
-        if response[-4:] != b'OK\r\n':
-            raise RuntimeError("Not OK")
-        return response[:-4]
+    def at_response(self, at_cmd, timeout=5, retries=3):
+        for _ in range(retries):
+            time.sleep(1)
+            if self._debug:
+                print("--->", at_cmd)
+            #self._uart.reset_input_buffer()
+            self._uart.write(at_cmd.encode('utf-8'))
+            self._uart.write(b'\x0d\x0a')
+            #uart.timeout = timeout
+            #print(uart.readline())  # read echo and toss
+            t = time.monotonic()
+            response = b''
+            while (time.monotonic() - t) < timeout:
+                response += self._uart.read(self._uart.in_waiting)
+                if response[-4:] == b'OK\r\n':
+                    break
+            # eat beginning \n and \r
+            if self._debug:
+                print("<---", response)
+            if response[-4:] != b'OK\r\n':
+                continue
+            return response[:-4]
+        raise RuntimeError("Not OK")
 
     def echo(self, e):
         if e:
-            self.at_response("ATE1", timeout=0.1)
+            self.at_response("ATE1", timeout=0.5)
         else:
-            self.at_response("ATE0", timeout=0.1)
+            self.at_response("ATE0", timeout=0.5)
 
 
     def soft_reset(self):
         try:
             self._uart.reset_input_buffer()
-            reply = self.at_response("AT+RST", timeout=0.1)
+            reply = self.at_response("AT+RST", timeout=0.5)
             if reply.strip(b'\r\n') == b'AT+RST':
+                time.sleep(2)
+                self._uart.reset_input_buffer()
                 return True
         except:
             pass # fail, see below
---
+++ examples/espatcommand_simpletest.py
@@ -0,0 +1,26 @@
+import time
+import board
+import busio
+
+from digitalio import DigitalInOut, Direction, Pull
+from adafruit_espatcommands import adafruit_espatcommands
+
+uart = busio.UART(board.TX, board.RX, baudrate=115200, timeout=1)
+resetpin = DigitalInOut(board.D3)
+
+print("ESP AT commands")
+esp = adafruit_espatcommands.espatcommands(uart, 115200, reset_pin = resetpin, debug=True)
+
+if not esp.soft_reset():
+    esp.hard_reset()
+    esp.soft_reset()
+
+esp.echo(False)
+print("Connected to AT software version ", esp.get_version())
+if esp.mode != esp.MODE_STATION:
+    esp.mode = esp.MODE_STATION
+print("Mode is now", esp.mode)
+for ap in esp.scan_APs():
+    print(ap)
+esp.join_AP("adafruit", "ffffffff")
+print("My IP Address:", esp.local_ip)
