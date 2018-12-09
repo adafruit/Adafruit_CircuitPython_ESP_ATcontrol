50eb0b2846e54dd9572628c5c77c0e5708994025
Jerry Needell <jerryneedell@gmail.com>
2018-12-09 17:13:48 -0500

    update simpletest

--- examples/espatcommand_simpletest.py
+++ examples/espatcommand_simpletest.py
@@ -6,13 +6,13 @@ MY_SSID = "foo"
 MY_PASSWORD = "bar"
 
 from digitalio import DigitalInOut, Direction, Pull
-from adafruit_espatcommands import adafruit_espatcommands
+from adafruit_espatcommands import espatcommands
 
 uart = busio.UART(board.TX, board.RX, baudrate=115200, timeout=1)
-resetpin = DigitalInOut(board.D3)
+resetpin = DigitalInOut(board.D5)
 
 print("ESP AT commands")
-esp = adafruit_espatcommands.espatcommands(uart, 115200, reset_pin = resetpin, debug=True)
+esp = espatcommands(uart, 115200, reset_pin = resetpin, debug=True)
 
 if not esp.soft_reset():
     esp.hard_reset()
