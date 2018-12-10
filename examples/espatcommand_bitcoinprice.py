17c76387950554a66faee323c540bc751c7c11a6
ladyada <limor@ladyada.net>
2018-12-09 23:49:26 -0500

    retry connecting if we have to

--- examples/espatcommand_bitcoinprice.py
+++ examples/espatcommand_bitcoinprice.py
@@ -2,14 +2,14 @@ import time
 import board
 import busio
 from digitalio import DigitalInOut, Direction, Pull
-import adafruit_espatcommands
+from adafruit_espatcommands import adafruit_espatcommands
 import ujson
 from adafruit_ht16k33 import segments
 import gc
 
 
-MY_SSID = "my ssid"
-MY_PASS = "the password"
+MY_SSID = "adafruit"
+MY_PASS = "password"
 #URL = "http://wifitest.adafruit.com/testwifi/index.html"
 URL = "http://api.coindesk.com/v1/bpi/currentprice.json"
 
@@ -21,19 +21,21 @@ i2c = busio.I2C(board.SCL, board.SDA)
 display = segments.Seg7x4(i2c)
 display.print('----')
 
-print("Get bitcoin")
+print("Get bitcoin price online")
 print("Free memory:", gc.mem_free() / 1024)
+
 esp = adafruit_espatcommands.espatcommands(uart, 115200, reset_pin = resetpin, debug=True)
 print("Connected to AT software version ", esp.get_version())
-
-time.sleep(3)
-
-esp.join_AP(MY_SSID, MY_PASS)
-print("My IP Address:", esp.local_ip)
 
 while True:
     try:
         display.print('----')
+        # Connect to WiFi if not already
+        if esp.remote_AP != MY_SSID:
+            esp.join_AP(MY_SSID, MY_PASS)
+            print("My IP Address:", esp.local_ip)
+
+        # great, lets get the JSON data
         header, body = esp.request_url(URL)
         json = ujson.loads(body)
         bitcoin = json["bpi"]["USD"]["rate_float"]
