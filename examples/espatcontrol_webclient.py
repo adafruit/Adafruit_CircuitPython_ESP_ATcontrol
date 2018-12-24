5d11071094ca5f47ceaefba4b4de7bc45157a5c1
ladyada <limor@ladyada.net>
2018-12-23 20:59:29 -0500

    updated to new style

--- examples/espatcontrol_webclient.py
+++ examples/espatcontrol_webclient.py
@@ -1,41 +1,44 @@
-import time
 import board
 import busio
+import time
 from digitalio import DigitalInOut
 import adafruit_espatcontrol
 
-MY_SSID = "my ssid"
-MY_PASS = "password"
+# Get wifi details and more from a settings.py file
+try:
+    from settings import settings
+except ImportError:
+    print("WiFi settings are kept in settings.py, please add them there!")
+    raise
 
-URL = "http://wifitest.adafruit.com/testwifi/index.html"
-
-uart = busio.UART(board.TX, board.RX, baudrate=115200, timeout=0.1)
+uart = busio.UART(board.TX, board.RX, timeout=0.1)
 resetpin = DigitalInOut(board.D5)
 
-print("Get a URL:", URL)
+URL = "http://wifitest.adafruit.com/testwifi/index.html"
+print("ESP AT GET URL", URL)
 
-esp = adafruit_espatcontrol.ESP_ATcontrol(uart, 115200, reset_pin=resetpin, debug=False)
-print("Connected to AT software version", esp.get_version())
+esp = adafruit_espatcontrol.ESP_ATcontrol(uart, 115200, run_baudrate=9600,
+                                          reset_pin=resetpin, debug=False)
+print("Resetting ESP module")
+esp.hard_reset()
 
 while True:
     try:
-        # Connect to WiFi if not already
-        AP = esp.remote_AP
-        print("Connected to", AP)
-        if AP[0] != MY_SSID:
-            esp.join_AP(MY_SSID, MY_PASS)
-            print("My IP Address:", esp.local_ip)
+        print("Checking connection...")
+        while not esp.is_connected:
+            print("Connecting...")
+            esp.connect(settings)
+            print("Connected to AT software version ", esp.version)
         # great, lets get the data
         print("Retrieving URL...", end='')
         header, body = esp.request_url(URL)
         print("OK")
-    except RuntimeError as e:
-        print("Failed to connect, retrying")
-        print(e)
+        
+        print('-'*40)
+        print(str(body, 'utf-8'))
+        print('-'*40)
+
+        time.sleep(60)
+    except (RuntimeError, adafruit_espatcontrol.OKError) as e:
+        print("Failed to get data, retrying\n", e)
         continue
-
-    print('-'*40)
-    print(str(body, 'utf-8'))
-    print('-'*40)
-
-    time.sleep(60)
