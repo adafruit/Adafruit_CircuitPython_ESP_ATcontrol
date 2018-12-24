99f47c37b7e899e8ba4efeb64eeb25bff00a838a
ladyada <limor@ladyada.net>
2018-12-23 21:55:17 -0500

    example setting file

---
+++ examples/espatcontrol_settings.py
@@ -0,0 +1,9 @@
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+settings = {
+    'ssid' : 'my access point',
+    'password' : 'hunter2',
+    'timezone' : -5, # this is offset from UTC
+    'github_token' : 'abcdefghij0123456789',
+}
