39f76bf9c06a164b80eb849d050bed4aa0e34325
brentru <robots199@me.com>
2019-01-02 14:05:36 -0500

    adding particle argon pinouts to simpletest and webclient

--- examples/espatcontrol_webclient.py
+++ examples/espatcontrol_webclient.py
@@ -11,8 +11,19 @@ except ImportError:
     print("WiFi settings are kept in settings.py, please add them there!")
     raise
 
+# With a Metro or Feather M4
 uart = busio.UART(board.TX, board.RX, timeout=0.1)
 resetpin = DigitalInOut(board.D5)
+
+# With a Particle Argon
+"""
+uart = busio.UART(board.ESP_RX, board.ESP_TX, timeout=0.1)
+resetpin = DigitalInOut(board.ESP_WIFI_EN)
+esp_boot = DigitalInOut(board.ESP_BOOT_MODE)
+from digitalio import Direction
+esp_boot.direction = Direction.OUTPUT
+esp_boot.value = True
+"""
 
 URL = "http://wifitest.adafruit.com/testwifi/index.html"
 print("ESP AT GET URL", URL)
