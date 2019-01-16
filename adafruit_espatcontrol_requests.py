b93f667cf720e24a384d4c105d1cab5ad48df8e2
ladyada <limor@ladyada.net>
2019-01-16 16:11:55 -0500

    lint2

--- adafruit_espatcontrol_requests.py
+++ adafruit_espatcontrol_requests.py
@@ -11,16 +11,18 @@ author='Paul Sokolovsky'
 license='MIT'
 """
 
-import adafruit_espatcontrol_socket as socket
 import gc
+import adafruit_espatcontrol_socket as socket
 
-_the_interface = None
+_the_interface = None   # pylint: disable=invalid-name
 def set_interface(iface):
-    global _the_interface
+    """Helper to set the global internet interface"""
+    global _the_interface   # pylint: disable=invalid-name, global-statement
     _the_interface = iface
     socket.set_interface(iface)
 
 class Response:
+    """The response from a request, contains all the headers/content"""
     headers = {}
     encoding = None
 
@@ -28,16 +30,20 @@ class Response:
         self.raw = f
         self.encoding = "utf-8"
         self._cached = None
+        self.status_code = None
+        self.reason = None
 
     def close(self):
+        """Close, delete and collect the response data"""
         if self.raw:
             self.raw.close()
             del self.raw
-        del self._cached 
+        del self._cached
         gc.collect()
 
     @property
     def content(self):
+        """The HTTP content direct from the socket, as bytes"""
         if self._cached is None:
             try:
                 self._cached = self.raw.read()
@@ -48,15 +54,23 @@ class Response:
 
     @property
     def text(self):
+        """The HTTP content, encoded into a string according to the HTTP
+        header encoding"""
         return str(self.content, self.encoding)
 
     def json(self):
+        """The HTTP content, parsed into a json dictionary"""
         import ujson
         return ujson.loads(self.content)
 
 
-def request(method, url, data=None, json=None, headers={}, stream=None):
-    global _the_interface
+# pylint: disable=too-many-branches, too-many-statements, unused-argument, too-many-arguments, too-many-locals
+def request(method, url, data=None, json=None, headers=None, stream=None):
+    """Perform an HTTP request to the given url which we will parse to determine
+    whether to use SSL ('https://') or not. We can also send some provided 'data'
+    or a json dictionary which we will stringify. 'headers' is optional HTTP headers
+    sent along. 'stream' is unused in this implementation"""
+    global _the_interface   # pylint: disable=global-statement, invalid-name
 
     try:
         proto, dummy, host, path = url.split("/", 3)
@@ -74,87 +88,92 @@ def request(method, url, data=None, json=None, headers={}, stream=None):
         host, port = host.split(":", 1)
         port = int(port)
 
-    ai = socket.getaddrinfo(host, port, 0, socket.SOCK_STREAM)
-    ai = ai[0]
-
-    s = socket.socket(ai[0], ai[1], ai[2])
-    resp = Response(s)  # our response
+    addr_info = socket.getaddrinfo(host, port, 0, socket.SOCK_STREAM)[0]
+    sock = socket.socket(addr_info[0], addr_info[1], addr_info[2])
+    resp = Response(sock)  # our response
 
     try:
         conntype = _the_interface.TYPE_TCP
         if proto == "https:":
             conntype = _the_interface.TYPE_SSL
-        s.connect(ai[-1], conntype)
-        s.write(b"%s /%s HTTP/1.0\r\n" % (method, path))
-        if not "Host" in headers:
-            s.write(b"Host: %s\r\n" % host)
-        if not "User-Agent" in headers:
-            s.write(b"User-Agent: Adafruit CircuitPython\r\n")
+        sock.connect(addr_info[-1], conntype)
+        sock.write(b"%s /%s HTTP/1.0\r\n" % (method, path))
+        if "Host" not in headers:
+            sock.write(b"Host: %s\r\n" % host)
+        if "User-Agent" not in headers:
+            sock.write(b"User-Agent: Adafruit CircuitPython\r\n")
         # Iterate over keys to avoid tuple alloc
         for k in headers:
-            s.write(k)
-            s.write(b": ")
-            s.write(headers[k])
-            s.write(b"\r\n")
+            sock.write(k)
+            sock.write(b": ")
+            sock.write(headers[k])
+            sock.write(b"\r\n")
         if json is not None:
             assert data is None
             import ujson
             data = ujson.dumps(json)
-            s.write(b"Content-Type: application/json\r\n")
+            sock.write(b"Content-Type: application/json\r\n")
         if data:
-            s.write(b"Content-Length: %d\r\n" % len(data))
-        s.write(b"\r\n")
+            sock.write(b"Content-Length: %d\r\n" % len(data))
+        sock.write(b"\r\n")
         if data:
-            s.write(data)
+            sock.write(data)
 
-        l = s.readline()
-        #print(l)
-        l = l.split(None, 2)
-        status = int(l[1])
+        line = sock.readline()
+        #print(line)
+        line = line.split(None, 2)
+        status = int(line[1])
         reason = ""
-        if len(l) > 2:
-            reason = l[2].rstrip()
+        if len(line) > 2:
+            reason = line[2].rstrip()
         while True:
-            l = s.readline()
-            if not l or l == b"\r\n":
+            line = sock.readline()
+            if not line or line == b"\r\n":
                 break
 
-            #print(l)
-            title, content = l.split(b': ', 1)
+            #print(line)
+            title, content = line.split(b': ', 1)
             if title and content:
                 title = str(title.lower(), 'utf-8')
                 content = str(content, 'utf-8')
                 resp.headers[title] = content
 
-            if l.startswith(b"Transfer-Encoding:"):
-                if b"chunked" in l:
-                    raise ValueError("Unsupported " + l)
-            elif l.startswith(b"Location:") and not 200 <= status <= 299:
+            if line.startswith(b"Transfer-Encoding:"):
+                if b"chunked" in line:
+                    raise ValueError("Unsupported " + line)
+            elif line.startswith(b"Location:") and not 200 <= status <= 299:
                 raise NotImplementedError("Redirects not yet supported")
 
     except OSError:
-        s.close()
+        sock.close()
         raise
 
     resp.status_code = status
     resp.reason = reason
     return resp
-
+# pylint: enable=too-many-branches, too-many-statements, unused-argument
+# pylint: enable=too-many-arguments, too-many-locals
 
 def head(url, **kw):
+    """Send HTTP HEAD request"""
     return request("HEAD", url, **kw)
 
 def get(url, **kw):
+    """Send HTTP GET request"""
     return request("GET", url, **kw)
 
 def post(url, **kw):
+    """Send HTTP POST request"""
     return request("POST", url, **kw)
 
 def put(url, **kw):
+    """Send HTTP PUT request"""
     return request("PUT", url, **kw)
 
 def patch(url, **kw):
+    """Send HTTP PATCH request"""
     return request("PATCH", url, **kw)
 
 def delete(url, **kw):
+    """Send HTTP DELETE request"""
     return request("DELETE", url, **kw)
