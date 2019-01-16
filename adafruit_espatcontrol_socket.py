9c93f348f36fe0f896a4db90f955ab7e855782f6
ladyada <limor@ladyada.net>
2019-01-16 16:48:27 -0500

    lint3

--- adafruit_espatcontrol_socket.py
+++ adafruit_espatcontrol_socket.py
@@ -1,23 +1,30 @@
 """A 'socket' compatible interface thru the ESP AT command set"""
+from micropython import const
 
-_the_interface = None
+_the_interface = None   # pylint: disable=invalid-name
 def set_interface(iface):
-    global _the_interface
+    """Helper to set the global internet interface"""
+    global _the_interface   # pylint: disable=global-statement, invalid-name
     _the_interface = iface
 
 SOCK_STREAM = const(1)
 AF_INET = const(2)
 
-def getaddrinfo(host, port,  # pylint: disable=too-many-arguments
-                family=0, socktype=0, proto=0, flags=0): # pylint: disable=unused-argument
+# pylint: disable=too-many-arguments, unused-argument
+def getaddrinfo(host, port, family=0, socktype=0, proto=0, flags=0):
     """Given a hostname and a port name, return a 'socket.getaddrinfo'
     compatible list of tuples. Honestly, we ignore anything but host & port"""
     if not isinstance(port, int):
         raise RuntimeError("port must be an integer")
     ipaddr = _the_interface.nslookup(host)
     return [(AF_INET, socktype, proto, '', (ipaddr, port))]
+# pylint: enable=too-many-arguments, unused-argument
 
+
+# pylint: disable=unused-argument, redefined-builtin, invalid-name
 class socket:
+    """A simplified implementation of the Python 'socket' class, for connecting
+    through an interface to a remote device"""
     def __init__(self, family=AF_INET, type=SOCK_STREAM, proto=0, fileno=None):
         if family != AF_INET:
             raise RuntimeError("Only AF_INET family supported")
@@ -26,15 +33,19 @@ class socket:
         self._buffer = b''
 
     def connect(self, address, conntype=None):
+        """Connect the socket to the 'address' (which should be dotted quad IP). 'conntype'
+        is an extra that may indicate SSL or not, depending on the underlying interface"""
         host, port = address
         if not _the_interface.socket_connect(conntype, host, port, keepalive=10, retries=3):
             raise RuntimeError("Failed to connect to host", host)
         self._buffer = b''
 
-    def write(self, data):
+    def write(self, data):         # pylint: disable=no-self-use
+        """Send some data to the socket"""
         _the_interface.socket_send(data)
 
     def readline(self):
+        """Attempt to return as many bytes as we can up to but not including '\r\n'"""
         if b'\r\n' not in self._buffer:
             # there's no line already in there, read some more
             self._buffer = self._buffer + _the_interface.socket_receive(timeout=3)
@@ -43,6 +54,8 @@ class socket:
         return firstline
 
     def read(self, num=0):
+        """Read up to 'num' bytes from the socket, this may be buffered internally!
+        If 'num' isnt specified, return everything in the buffer."""
         if num == 0:
             # read as much as we can
             ret = self._buffer + _the_interface.socket_receive(timeout=1)
@@ -53,6 +66,8 @@ class socket:
         return ret
 
     def close(self):
+        """Close the socket, after reading whatever remains"""
         # read whatever's left
         self._buffer = self._buffer + _the_interface.socket_receive(timeout=1)
         _the_interface.socket_disconnect()
+# pylint: enable=unused-argument, redefined-builtin, invalid-name
