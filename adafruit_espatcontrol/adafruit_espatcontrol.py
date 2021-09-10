f267f01042e4837e64e005f79e49a88ee2d10317
Pontus Oldberg <pontus.oldberg@invector.se>
2021-09-10 15:39:46 +0200

    Read larger chunks to avoid loosing data.

--- adafruit_espatcontrol/adafruit_espatcontrol.py
+++ adafruit_espatcontrol/adafruit_espatcontrol.py
@@ -523,7 +523,7 @@ class ESP_ATcontrol:
             response = b""
             while (time.monotonic() - stamp) < timeout:
                 if self._uart.in_waiting:
-                    response += self._uart.read(1)
+                    response += self._uart.read(self._uart.in_waiting)
                     self.hw_flow(False)
                     if response[-4:] == b"OK\r\n":
                         break
