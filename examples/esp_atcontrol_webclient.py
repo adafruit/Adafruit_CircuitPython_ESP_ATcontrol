0786a08c87b17a506c5b3cc142b32910cd64dc65
mperino <mark.perino@gmail.com>
2021-11-29 11:41:48 -0800

    Update esp_atcontrol_webclient.py
    Added support for Challenger RP2040, added a variable for setting the Time Between Queries, and print when it sleeps..

--- examples/esp_atcontrol_webclient.py
+++ examples/esp_atcontrol_webclient.py
@@ -18,21 +18,37 @@ except ImportError:
     print("WiFi secrets are kept in secrets.py, please add them there!")
     raise
 
+# Debug Level
+# Change the Debug Flag if you have issues with AT commands
+debugflag = False
 
-# With a Particle Argon
-RX = board.ESP_TX
-TX = board.ESP_RX
-resetpin = DigitalInOut(board.ESP_WIFI_EN)
-rtspin = DigitalInOut(board.ESP_CTS)
-uart = busio.UART(TX, RX, timeout=0.1)
-esp_boot = DigitalInOut(board.ESP_BOOT_MODE)
-esp_boot.direction = Direction.OUTPUT
-esp_boot.value = True
+# How long between queries
+TIME_BETWEEN_QUERY = 60  # in seconds
 
+if board.board_id == "challenger_rp2040_wifi":
+    RX = board.ESP_RX
+    TX = board.ESP_TX
+    resetpin = DigitalInOut(board.WIFI_RESET)
+    rtspin = False
+    uart = busio.UART(TX, RX, baudrate=11520, receiver_buffer_size=2048)
+    esp_boot = DigitalInOut(board.WIFI_MODE)
+    esp_boot.direction = Direction.OUTPUT
+    esp_boot.value = True
+    status_light = None
+else:
+    RX = board.ESP_TX
+    TX = board.ESP_RX
+    resetpin = DigitalInOut(board.ESP_WIFI_EN)
+    rtspin = DigitalInOut(board.ESP_CTS)
+    uart = busio.UART(TX, RX, timeout=0.1)
+    esp_boot = DigitalInOut(board.ESP_BOOT_MODE)
+    esp_boot.direction = Direction.OUTPUT
+    esp_boot.value = True
+    status_light = None
 
 print("ESP AT commands")
 esp = adafruit_espatcontrol.ESP_ATcontrol(
-    uart, 115200, reset_pin=resetpin, rts_pin=rtspin, debug=False
+    uart, 115200, reset_pin=resetpin, rts_pin=rtspin, debug=debugflag
 )
 
 URL = "http://wifitest.adafruit.com/testwifi/index.html"
@@ -57,8 +73,8 @@ while True:
         print("Content size:", r.headers["content-length"])
         print("Encoding:", r.encoding)
         print("Text:", r.text)
-
-        time.sleep(60)
+        print("Sleeping for: {0} Seconds".format(TIME_BETWEEN_QUERY))
+        time.sleep(TIME_BETWEEN_QUERY)
     except (ValueError, RuntimeError, adafruit_espatcontrol.OKError) as e:
         print("Failed to get data, retrying\n", e)
         continue
